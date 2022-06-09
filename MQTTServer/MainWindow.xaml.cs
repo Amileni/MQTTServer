dd893ec1b2547a75c439e114d6c1dff1f6c47d40
Amileni <aminie@nbstech.eu.com>
2022-06-09 16:29:19 +0200

    Ajout client handler

--- MQTTServer/MainWindow.xaml.cs
+++ MQTTServer/MainWindow.xaml.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+﻿using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MQTTnet;
+using MQTTnet.Server;
+using MQTTnet.Protocol;
+using MQTTnet.Formatter;
+using MQTTnet.Implementations;
+using MQTTnet.Client;
 using MahApps.Metro.Controls;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace MQTTServer
 {
@@ -24,6 +21,24 @@ namespace MQTTServer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public static async Task Run_Server()
+        {
+            var server = new MqttFactory().CreateMqttServer();
+            var serverOptions = new MqttServerOptionsBuilder()
+                                    .WithDefaultEndpoint()
+                                    .WithDefaultEndpointPort(707)
+                                    .WithApplicationMessageInterceptor(OnNewMessage);
+
+            await server.StartAsync(serverOptions.Build());
+        }
+
+        public static void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        {
+            var payload = context.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+
+
         }
     }
 }
